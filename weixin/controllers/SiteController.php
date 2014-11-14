ed984cbbc108153004c5a9d3768a78b970ab9ef9
bobchengbin <bob@phpor.me>
2014-11-14 10:57:34 +0800

    微信接口测试

--- weixin/controllers/SiteController.php
+++ weixin/controllers/SiteController.php
@@ -3,6 +3,7 @@ namespace weixin\controllers;
 
 use Yii;
 use yii\web\Controller;
+use weixin\models\Weixin;
 
 /**
  * Site controller
@@ -11,15 +12,11 @@ class SiteController extends Controller
 {
     public function actionIndex()
     {
-        // url: weixin.bobdarex.phpor.me/site 或 weixin.bobdarex.phpor.me
-        return Yii::$app->request->get('echostr');
-    }
-    public function actionBob(){
-        // url: weixin.bobdarex.phpor.me/site/bob
-        return 'site/bob';
-    }
-    public function actionBobTest(){
-        // url: weixin.bobdarex.phpor.me/site/bob-test
-        return 'site/bob-test。。。。。';
+        $model = new Weixin;
+        if(!$model->checkSignature())
+            exit("check signature error.");
+
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        return $model->responseMsg($postStr);
     }
 }
---
+++ weixin/models/Weixin.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bob
+ * Date: 14/11/14
+ * Time: 上午10:01
+ */
+
+namespace weixin\models;
+
+use Yii;
+use yii\base\Model;
+
+class Weixin extends Model{
+    const TOKEN = "bobdarex";
+    const ENCODING_AES_KEY = "T9FwIpxZpTg3M058qeBXZVXJgIJbn7JUj0HaU9ntVlP";
+
+    public function responseMsg($postStr=""){
+
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+            if(!empty( $keyword )) {
+                // 如果用户有输入内容，则我们就回复。
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                return sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            }else{
+                return "Input something...";
+            }
+
+        }
+        return "";
+    }
+
+    /**
+     * 检查signature的准确性
+     *
+     * @return boolean
+     */
+    public function checkSignature(){
+        $signature = Yii::$app->request->get('signature');
+        $timestamp = Yii::$app->request->get('timestamp');
+        $nonce =Yii::$app->request->get('nonce');
+
+        $generatorSignature = $this->_generatorSignature($timestamp, $nonce, self::TOKEN);
+        return $generatorSignature == $signature;
+    }
+
+    /**
+     * 生成Signature的方法，这个应该和微信那里的算法是一致的，这样才能得到相同的结果。
+     *
+     * @param $timestamp 微信那边传过来的时间戳
+     * @param $nonce
+     * @param string|微信后台设置的token值 $token 微信后台设置的token值
+     * @return string
+     */
+    private function _generatorSignature($timestamp, $nonce, $token = self::TOKEN){
+
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        return sha1(implode($tmpArr));
+    }
+}
